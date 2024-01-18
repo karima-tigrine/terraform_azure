ec15e70202cda6fcfb4af2b96e0e40661145e8d1
karima-tigrine <karinetigrine@gmail.com>
2024-01-18 14:53:40 +0000

    correct outputs.tf file

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,3 @@ output "public_ip_fqdn" {
   description = "The FQDN of the public IP address."
   value       = azurerm_public_ip.lb_pubip.fqdn
 }
-
-
-
-output "" {
-
-    value = azurerm_linux_virtual_machine.vm.
-}
